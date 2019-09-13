6a6edc157958c23478381f08ab6693916487fce1
edu <x>
2019-09-12 21:58:13 -0300

    criando classe

--- Escola/src/escola/CriarAluno.java
+++ Escola/src/escola/CriarAluno.java
@@ -3,7 +3,11 @@ package escola;
 public class CriarAluno {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Aluno aluno1 = new Aluno();
+	aluno1.nome = "Juliana"
+	aluni1.idade = 34;
+	aluno1.nota1 = 7.5;
+	aluno.nota2 = 8.2;
 
 	}
 
